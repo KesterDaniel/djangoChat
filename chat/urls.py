2855c4c3d5ca49c2f0b953e7764b0b9ac8a172cb
KesterDaniel <kesterdan17@gmail.com>
2022-12-21 18:11:16 +0100

    added room functionality

--- chat/urls.py
+++ chat/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
   path("", views.home, name="index"),
+  path("checkview", views.checkview, name="checkview"),
+  path("<str:room>/", views.room, name="room"),
+  path("send", views.send, name="send")
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room, Message
 
 # Create your views here.
 
 def home(request):
   return render(request, "home.html")
 
+def room(request, room):
+  username = request.GET.get("username")
+  room_details = Room.objects.get(name=room)
+  return render(request, "room.html", {"room_details": room_details, "room": room, "username": username})
+
 def checkview(request):
   room = request.POST["room_name"]
   username = request.POST["username"]
 
-  pass
+  if Room.objects.filter(name=room).exists():
+    return redirect("/" + room + "/?username=" + username)
+  else:
+    new_room = Room.objects.create(name=room)
+    new_room.save()
+    return redirect("/" + room + "/?username=" + username)
